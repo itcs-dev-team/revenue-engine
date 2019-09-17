4fc1c2b3989a017ef55d42e92c1a79213ef01d32
Aaron Law <aaronlaw@gmail.com>
2019-09-17 16:17:05 +0800

    Route services.html.

--- mysite/urls.py
+++ mysite/urls.py
@@ -6,4 +6,5 @@ app_name = "mysite"
 urlpatterns = [
     # Home page
     path('', views.index, name='index'),
+    path('services/', views.services, name='service'),
 ]
--- mysite/views.py
+++ mysite/views.py
@@ -5,6 +5,11 @@ from datetime import datetime
 # Create your views here.
 def index(request):
     """The home page of mysite."""
-    now = datetime.now()
-    current_time = {'current_time': str(now)}
-    return render(request, 'mysite/index.html', current_time)
+    # now = datetime.now()
+    # current_time = {'current_time': str(now)}
+    # return render(request, 'mysite/index.html', current_time)
+    return render(request, 'mysite/index.html')
+
+def services(request):
+    """The service page."""
+    return render(request, 'mysite/services.html')
