28d8e2637653f97075679362cc40f4def795382c
Aaron Law <aaronlaw@gmail.com>
2019-10-21 09:34:20 +0800

    Finish exp.

--- revenue_engine/urls.py
+++ revenue_engine/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('mysite.urls', namespace='mysite')), # General site
+    path('services/', include('services.urls', namespace='services')), # General site
 ]
