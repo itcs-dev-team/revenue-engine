c96f350f721e56135c1540941c9f244cd4934e6b
Aaron Law <aaronlaw@gmail.com>
2019-10-22 16:32:00 +0800

    Add models location in mysite and makemigrations.

--- mysite/models.py
+++ mysite/models.py
@@ -1,3 +1,64 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.conf import settings
+from django.utils import timezone
 
+User = settings.AUTH_USER_MODEL
 # Create your models here.
+
+## Ref: https://docs.djangoproject.com/zh-hans/2.2/topics/db/queries/
+# class Blog(models.Model):
+#     name = models.CharField(max_length=100)
+#     tagline = models.TextField()
+
+#     def __str__(self):
+#         return self.name
+
+# class Author(models.Model):
+#     name = models.CharField(max_length=200)
+#     email = models.EmailField()
+
+#     def __str__(self):
+#         return self.name
+
+# class Entry(models.Model):
+#     blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
+#     headline = models.CharField(max_length=255)
+#     body_text = models.TextField()
+#     pub_date = models.DateField()
+#     mod_date = models.DateField()
+#     authors = models.ManyToManyField(Author)
+#     n_comments = models.IntegerField()
+#     n_pingbacks = models.IntegerField()
+#     rating = models.IntegerField()
+
+#     def __str__(self):
+#         return self.headline
+
+class Status(models.Model):
+    STATUS = [
+        ('1', 'active'),
+        ('2', 'inactive'),
+        ('3', 'deleted'),
+    ]
+    Status = models.IntegerField(choices=STATUS, default=2)
+
+class Location(models.Model):
+    location = models.CharField(max_length=255, blank=True)
+    company_name = models.CharField(max_length=255, blank=True)
+    jp_company_name = models.CharField(max_length=255, blank=True)
+    address = models.CharField(max_length=255, blank=True)
+    jp_address = models.CharField(max_length=255, blank=True)
+    telephone = models.CharField(max_length=30, blank=True)
+    fax = models.CharField(max_length=30, blank=True)
+    email = models.EmailField(max_length=254, blank=True)
+    picture = models.ImageField(max_length=100, blank=True)
+    ea_licence = models.CharField(max_length=255, null=True)
+    labor_dept_url = models.URLField(max_length=200, null=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(User, default='1', on_delete=models.SET_DEFAULT)
+    # created_by = models.ForeignKey(settings.AUTH_USER_MODEL, default='1', on_delete=models.SET_DEFAULT)
+    status = models.ForeignKey(Status, on_delete=models.SET_DEFAULT, default=2)
+    
+    def __str__(self):
+        return self.location
