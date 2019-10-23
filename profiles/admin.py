d1c64cd990e64db3d55eddd2ee471c450e8d20c0
Aaron Law <aaronlaw@gmail.com>
2019-10-23 23:55:09 +0800

    Register ModelAdmin in profiles.

--- profiles/admin.py
+++ profiles/admin.py
@@ -2,15 +2,21 @@ from django.contrib import admin
 from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
 
-from .forms import CustomUserCreationForm, CustomUserChangeForm
-from .models import CustomUser
+from .forms import CustomUserCreationForm, CustomUserChangeForm # https://wsvincent.com/django-custom-user-model-tutorial/
 
 # Register your models here.
+from .models import CustomUser, AccountType
 
+# Register the Admin classes for JobType using the decorator
+# Reference: https://developer.mozilla.org/zh-TW/docs/Learn/Server-side/Django/Admin_site
+@admin.register(CustomUser) #admin.site.register(CustomUser, CustomUserAdmin)
 class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     model = CustomUser
     list_display = ['email', 'username',]
 
-admin.site.register(CustomUser, CustomUserAdmin)
+@admin.register(AccountType) # admin.site.register(JobType)
+class AccountTypeAdmin(admin.ModelAdmin):
+    pass
+
