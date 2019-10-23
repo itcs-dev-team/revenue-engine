e2e95c6ab5ac3c8c738aff6d8a9f23bc3f13a388
Aaron Law <aaronlaw@gmail.com>
2019-10-23 23:46:14 +0800

    Register AdminModel in mysite.

--- mysite/admin.py
+++ mysite/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Status, Location
+
+# Register the Admin classes for JobType using the decorator
+# Reference: https://developer.mozilla.org/zh-TW/docs/Learn/Server-side/Django/Admin_site
+@admin.register(Status) # admin.site.register(JobType)
+class StatusAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Location)
+class LocationAdmin(admin.ModelAdmin):
+    pass
