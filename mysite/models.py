5b24e3aee07b6f96e02b58f45b8cf5511542c753
Aaron Law <aaronlaw@gmail.com>
2019-10-24 17:50:37 +0800

    Give Model Status an verbose_name_plural.

--- mysite/models.py
+++ mysite/models.py
@@ -46,6 +46,10 @@ class Status(models.Model):
     
     def __str__(self):
         return self.status
+    
+    class Meta:
+        verbose_name_plural = 'status'
+        ordering = ['status']
 
 class Location(models.Model):
     location        = models.CharField(max_length=255, blank=True)
