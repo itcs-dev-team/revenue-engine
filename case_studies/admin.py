f736e9ce853d39349caf08085f1b685e99a4d82f
Aaron Law <aaronlaw@gmail.com>
2019-10-24 17:49:57 +0800

    Register case_studies in admin.

--- case_studies/admin.py
+++ case_studies/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import CaseStudy
+
+# Register the Admin classes for JobType using the decorator
+# Reference: https://developer.mozilla.org/zh-TW/docs/Learn/Server-side/Django/Admin_site
+@admin.register(CaseStudy) # admin.site.register(JobType)
+class CaseStudyAdmin(admin.ModelAdmin):
+    pass
