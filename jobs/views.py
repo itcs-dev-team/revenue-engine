ee802ca15bb43f557662b89449842aff87448c6e
Aaron Law <aaronlaw@gmail.com>
2019-10-28 09:34:18 +0800

    Refactor variable "jobs" to "posts".

--- jobs/views.py
+++ jobs/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views import View
-from django.views.generic import TemplateView, ListView, UpdateView
+from django.views.generic import TemplateView, ListView, DetailView
 
 from .models import Post
 # Create your views here.
@@ -9,21 +9,26 @@ from .models import Post
 #     return render(request, 'jobs/career.html')
 
 # level 2 pages
-
-# career section
 def apply_now(request):
     """The apply_now page."""
     return render(request, 'jobs/apply_now.html')
 
 ## CBV
-class CareerListView(ListView):
-    """The Career home page.
+class CareerListView(ListView): # TODO: Rename to JobListView.
+    """Show a list of jobs.
     
-    It shows list of jobs."""
+    The Career home page.
+    """
     model = Post
     template_name = "jobs/career_.html"
-    context_object_name = 'all_jobs'
-    
+    context_object_name = 'posts'
+
+class JobDetailView(DetailView):
+    """A job with its detail. Take an id and give a job."""
+    model = Post
+    template_name = "jobs/jobs_details_.html"
+    context_object_name = 'post'
+
 # class ApplyUpdateView(UpdateView):
 #     model = Post
 #     template_name = "jobs/apply_now.html"
